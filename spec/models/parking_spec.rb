f708edd9cee95c0486da8b1b3700c7eee6200a8e
eagle1111 <wang499817054@gmail.com>
2017-07-30 19:48:09 +0800

    rspec calculate

--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -6,15 +6,35 @@ RSpec.describe Parking, type: :model do
     it "is invalid without amount" do
       parking = Parking.new( :parking_type => "guest",
                              :start_at => Time.now - 6.hours,
-                             :end_at => Time.now)
+                             :end_at => Time.now )
       expect( parking ).to_not be_valid
     end
 
     it "is invalid without end_at" do
       parking = Parking.new( :parking_type => "guest",
                              :start_at => Time.now - 6.hours,
-                             :amount => 999)
+                             :amount => 999 )
       expect( parking ).to_not be_valid
+    end
+  end
+
+  describe ".calculate_amount" do
+    it "30 mins should be ¥2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest",
+                             :start_at => t,
+                             :end_at => t + 30.minutes )
+      parking.calculate_amount
+      expect( parking.amount ).to eq(200)
+    end
+
+    it "60 mins should be ¥2" do
+      t = Time.now
+      parking = Parking.new( :parking_type => "guest",
+                             :start_at => t,
+                             :end_at => t + 60.minutes )
+      parking.calculate_amount
+      expect( parking.amount ).to eq(200)
     end
   end
 end
